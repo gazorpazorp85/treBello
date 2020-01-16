304067f804f75fa6f5791e2bb6ca03d28e978a23
batalinvlad <batalinvlad@gmail.com>
2020-01-16 18:42:00 +0200

    new style

--- frontend/src/cmps/BoardColumns.js
+++ frontend/src/cmps/BoardColumns.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { withRouter } from 'react-router';
 import TasksList from './TasksList';
+// import Card from '@material-ui/core/Card';
 
 // import { Link } from 'react-router-dom';
 
 function BoardColumns({ columns }) {
     return (
-        < section >
-            <div>
-                {columns.map(column => (
-                    <div key={column.id}>
-                        {column.title}
-                        <TasksList tasks={column.tasks} />
-                        {/* <button onClick={props.onEditColumn}>Edit Column</button> */}
-                    </div>
-            ))}
-            </div>
+        <div className="board-columns grid-container">
 
-        </section >
+            {columns.map(column => (
+                <div className="board-columns-grid-item" key={column.id}>
+                    <div className="board-columns-grid-item-header flex align-center space-between">
+                        <h2>{column.title}</h2>
+                        <p>
+                            X
+                           </p>
+                    </div>
+                    <TasksList tasks={column.tasks} />
+                    {/* <button onClick={props.onEditColumn}>Edit Column</button> */}
+                </div>
+            ))}
+
+        </div >
     )
 }
 
