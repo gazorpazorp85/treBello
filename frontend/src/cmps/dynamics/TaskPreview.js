239cd9ad9797c1efba4eb3031675c94a42c3e618
batalinvlad <batalinvlad@gmail.com>
2020-01-21 14:33:55 +0200

    pull

--- frontend/src/cmps/dynamics/TaskPreview.js
+++ frontend/src/cmps/dynamics/TaskPreview.js
@@ -1,28 +1,26 @@
 import React from "react";
 // import moment from 'moment';
 import CreateIcon from '@material-ui/icons/Create';
-import Card from '@material-ui/core/Card';
 
-// import 'moment/locale/es'
 
 export default function TaskPreview({ task, provided, innerRef, isDragging, style, onDelete, showEditBtn, onTaskId }) {
     return (
         <section>
-            <div
-                className={"task-container" + (isDragging ? " isDragging" : "")}
-                {...provided.draggableProps}
-                {...provided.dragHandleProps}
-                ref={innerRef}
-                style={style}
-            >
-                <p>{task.title}</p>
-                {(showEditBtn && (onTaskId === task.id)) ?
-                    <div className="task-container-open-menu-wrapper flex align-center justify-center">
-                        <CreateIcon className="task-container-open-menu"
-                            onClick={onDelete} />
-                    </div>
-                    : ''}
-            </div>
+                <div
+                    className={"task-container" + (isDragging ? " isDragging" : "")}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={innerRef}
+                    style={style}
+                >
+                    <p>{task.title}</p>
+                    {(showEditBtn && (onTaskId === task.id)) ?
+                        <div className="task-container-open-menu-wrapper flex align-center justify-center">
+                            <CreateIcon className="task-container-open-menu"
+                                onClick={onDelete} />
+                        </div>
+                        : ''}
+                </div>
         </section>
     )
 }
