7fee86db90c74a3d00b4b2e7578a1ea01039e36e
gazorpazorp85 <paolo.groppi@gmail.com>
2020-01-28 17:04:30 +0200

    bushbush

---
+++ backend/public/precache-manifest.22bd2c95c37a40c18da0531b0b5742fc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a91871512c43d13eebf9fbd3e49b8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b0d44b494530d1f8bb9",
+    "url": "/static/css/2.0d4cffde.chunk.css"
+  },
+  {
+    "revision": "90c306ab87ac8c2bf337",
+    "url": "/static/css/main.a7e5e1db.chunk.css"
+  },
+  {
+    "revision": "4b0d44b494530d1f8bb9",
+    "url": "/static/js/2.298f348c.chunk.js"
+  },
+  {
+    "revision": "15970c22f221d7496c0b959cd5a5c6d9",
+    "url": "/static/js/2.298f348c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90c306ab87ac8c2bf337",
+    "url": "/static/js/main.93a4bec6.chunk.js"
+  },
+  {
+    "revision": "c9f1875f7e246a67df7e",
+    "url": "/static/js/runtime-main.cb82405f.js"
+  },
+  {
+    "revision": "fafc43f57fea4265af848af5bfece323",
+    "url": "/static/media/Margad.fafc43f5.jpg"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "19ba7aa52a78c3896558ac1c0a5fb4c7",
+    "url": "/static/media/Montserrat-ExtraBold.19ba7aa5.ttf"
+  },
+  {
+    "revision": "570a244cacd3d78b8c75ac5dd622f537",
+    "url": "/static/media/Montserrat-ExtraLight.570a244c.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "4f7b984acc369795ef3a62b812b0da0c",
+    "url": "/static/media/Paolo.4f7b984a.jpg"
+  },
+  {
+    "revision": "3c3881bd58370cb34915a690ea04ee31",
+    "url": "/static/media/Vlad.3c3881bd.jpg"
+  },
+  {
+    "revision": "fb22104e1bdfb0afd62476691a00ff67",
+    "url": "/static/media/board-bg.fb22104e.jpg"
+  },
+  {
+    "revision": "f3e0e3e8107fce05b15036fc9dd459d7",
+    "url": "/static/media/boardtmp.f3e0e3e8.png"
+  },
+  {
+    "revision": "69ea95055e926a199371c27f8da4a494",
+    "url": "/static/media/header-bg.69ea9505.jpg"
+  }
+]);
