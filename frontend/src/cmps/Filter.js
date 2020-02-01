edabe07dd23fdf7f199c8693bf6f2722e071b252
batalinvlad <batalinvlad@gmail.com>
2020-02-01 20:05:23 +0200

    new pull

--- frontend/src/cmps/Filter.js
+++ frontend/src/cmps/Filter.js
@@ -10,22 +10,21 @@ export default class Filter extends Component {
     }
 
     inputChange = (ev) => {
-
         let fieldName = ev.target.name;
         let value = ev.target.value;
         this.setState(({ filterBy: { [fieldName]: value } }), () => this.props.onFilter(this.state.filterBy));
     }
 
     render() {
-
         const teamMembers = this.props.teamMembers;
-
         return <div className="board-page-nav-bar-filters-item flex">
+
             <input type="text" placeholder="Search card by name"
                 value={this.state.filterBy.title}
                 onChange={this.inputChange} name="title">
             </input>
-            <div style={{background: (this.props.isDarkBackground) ? 'white' : 'black'}}className="board-page-nav-bar-filters-divider"></div>
+
+            <div style={{background: (this.props.isDarkBackground) ? 'white' : 'black'}} className="board-page-nav-bar-filters-divider"></div>
             <select name="teamMembers" style={{
                     color: (this.props.isDarkBackground) ? 'white' : 'black',
                     background: (this.props.isDarkBackground) ? '#00000094' : '#ffffff4f'
