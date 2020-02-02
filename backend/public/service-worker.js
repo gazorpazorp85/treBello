d8924d49d4c210daaf6bf41a3ecb6209179b7d47
gazorpazorp85 <paolo.groppi@gmail.com>
2020-02-02 12:50:29 +0200

    sunday bushbushbush

--- backend/public/service-worker.js
+++ backend/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.23bab71da1cd60100d472214cb23c3a3.js"
+  "/precache-manifest.28be403f26fefc80bbb7bb903eb2c7f6.js"
 );
 
 self.addEventListener('message', (event) => {
